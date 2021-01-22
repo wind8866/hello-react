dcc458e9482c92449d9f1a42c38f7c7163788125
liuzhen <liuzhen1010xyz@gmail.com>
2021-01-22 15:31:33 +0800

    change3

--- my-redux/index.js
+++ my-redux/index.js
@@ -1,48 +1,67 @@
-
-
-const initState = (defaultState) => {
-  let state = defaultState;
-
-  const getState = () => state;
-  const dispatch = (action) => {
-    switch(action.type) {
-      case 'SET_TITLE':
-        state.title = action.title;
-        break;
-      case 'SET_CONTENT':
-        state.content = action.content;
-        break;
-      default:
-        console.info(new Error('No Actiion'));
-    }
-  }
-
-  return {
-    getState,
-    dispatch,
-  }
-}
-
-
-const renderTitle = (text) => {
+const renderTitle = (text, oldText = '') => {
+  if (oldText === text) { return }
+  console.log('render title')
   const rootDOM = document.querySelector('#title');
   rootDOM.innerText = text;
 };
 
-const renderContent = (text) => {
+const renderContent = (text, oldText = '') => {
+  if (oldText === text) { return }
+  console.log('render content')
   const rootDOM = document.querySelector('#content');
   rootDOM.innerText = text;
 };
 
-const { getState, dispatch } = initState({
+const render = (state, oldState = {}) => {
+  renderTitle(state.title, oldState.title);
+  renderContent(state.content, oldState.content);
+};
+
+const createStore = (defaultState, stateChanger) => {
+  let state = defaultState;
+  const listeners = [];
+  let oldState = state;
+  const getState = () => state;
+  const dispatch = (action) => {
+    oldState = state;
+    state = stateChanger(action, state);
+    listeners.forEach(listener => listener(state, oldState));
+  };
+  const subscribe = listener => listeners.push(listener);
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  }
+}
+
+const defaultState = {
   title: 'redux',
   content: 'This is my redux',
-});
-dispatch({type: 'SET_TITLE', title: 'react3'});
-
-const render = (state) => {
-  renderTitle(state.title);
-  renderContent(state.content);
 };
+const stateChanger = (action, state) => {
+  switch(action.type) {
+    case 'SET_TITLE':
+      return {
+        ...state,
+        title: action.title,
+      }
+      break;
+    case 'SET_CONTENT':
+      return {
+        ...state,
+        content: action.content,
+      }
+      break;
+    default:
+      console.info(new Error('No Actiion'));
+  }
+}
+const { getState, dispatch, subscribe } = createStore(defaultState, stateChanger);
+subscribe((state, oldState) => render(state, oldState));
+subscribe(() => console.log('render end'));
+
+
+dispatch({type: 'SET_TITLE', title: 'react3'});
 
 render(getState());
