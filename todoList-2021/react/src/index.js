34186256e963506a5bc896cfc0de4169a917b37f
liuzhen <liuzhen1010xyz@gmail.com>
2021-07-25 16:59:30 +0800

    redux-saga可运行

--- todoList-2021/react/src/index.js
+++ todoList-2021/react/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 import TodoAppObject from './components/react-object/index.js';
 import TodoAppHook from './components/react-hook/index.js';
+import TodoAppRedux from './components/react-redux-saga/index.js';
 import './components/react-hook/index.css';
 
-ReactDOM.render(<TodoAppHook/>, document.querySelector('#root'));
+ReactDOM.render(<TodoAppRedux/>, document.querySelector('#root'));
