90015f3560d3ffb01170abbdf7309942d92d09f2
liuzhen <liuzhen1010xyz@gmail.com>
2021-07-25 17:09:52 +0800

    加入 router，使其hook2可运行

--- todoList-2021/react/src/index.js
+++ todoList-2021/react/src/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 import TodoAppObject from './components/react-object/index.js';
 import TodoAppHook from './components/react-hook/index.js';
 import TodoAppRedux from './components/react-redux-saga/index.js';
+import TodoAppHook2 from './components/react-hook-2/index.js';
 import './components/react-hook/index.css';
 
-ReactDOM.render(<TodoAppRedux/>, document.querySelector('#root'));
+const App = () => (<div>
+  <Router>
+    <Link to="/demo0">TodoAppObject</Link> <br/>
+    <Link to="/demo1">TodoAppHook</Link> <br/>
+    <Link to="/demo2">TodoAppRedux</Link> <br/>
+    <Link to="/demo3">TodoAppHook2</Link> <br/>
+    <hr/>
+    <Switch>
+      <Route path="/demo0">
+        <h1>TodoAppObject</h1>
+        <TodoAppObject />
+      </Route>
+      <Route path="/demo1">
+        <h1>TodoAppHook</h1>
+        <TodoAppHook />
+      </Route>
+      <Route path="/demo2">
+        <h1>TodoAppRedux</h1>
+        <TodoAppRedux />
+      </Route>
+      <Route path="/demo3">
+        <h1>TodoAppHook2</h1>
+        <TodoAppHook2 />
+      </Route>
+      
+    </Switch>
+  </Router>
+</div>)
+
+ReactDOM.render(<App />, document.querySelector('#root'));
